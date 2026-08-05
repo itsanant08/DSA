abdb9a9c86630073d6b27ab544de6eb082876f1e
Anant Pratap <itsanant08@gmail.com>
2026-08-05 10:12:22 +0530

    leetcode code added for mergeinterval

---
+++ mergeinterval.cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end());
+
+        vector<vector<int>> ans;
+        ans.push_back(intervals[0]);
+
+        for (int i = 1; i < intervals.size(); i++) {
+            if (intervals[i][0] <= ans.back()[1]) {
+                ans.back()[1] = max(ans.back()[1], intervals[i][1]);
+            } else {
+                ans.push_back(intervals[i]);
+            }
+        }
+
+        return ans;
+    }
+};
+
+int main() {
+    vector<vector<int>> intervals = {
+        {1, 3},
+        {2, 6},
+        {8, 10},
+        {15, 18}
+    };
+
+    Solution obj;
+    vector<vector<int>> result = obj.merge(intervals);
+
+    cout << "Merged Intervals:" << endl;
+    for (const auto &interval : result) {
+        cout << "[";
+        for (int i = 0; i < interval.size(); i++) {
+            cout << interval[i];
+            if (i != interval.size() - 1)
+                cout << ", ";
+        }
+        cout << "]" << endl;
+    }
+
+    return 0;
+}
+
+
+
+/*for leetcode*/
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end());
+
+        vector<vector<int>> ans;
+        ans.push_back(intervals[0]);
+
+        for (int i = 1; i < intervals.size(); i++) {
+            if (intervals[i][0] <= ans.back()[1]) {
+                ans.back()[1] = max(ans.back()[1], intervals[i][1]);
+            } else {
+                ans.push_back(intervals[i]);
+            }
+        }
+
+        return ans;
+    }
+};
+
+int main() {
+    vector<vector<int>> intervals = {
+        {1, 3},
+        {2, 6},
+        {8, 10},
+        {15, 18}
+    };
+
+    Solution obj;
+    vector<vector<int>> result = obj.merge(intervals);
+
+    cout << "Merged Intervals:" << endl;
+    for (const auto &interval : result) {
+        cout << "[";
+        for (int i = 0; i < interval.size(); i++) {
+            cout << interval[i];
+            if (i != interval.size() - 1)
+                cout << ", ";
+        }
+        cout << "]" << endl;
+    }
+
+    return 0;
+}
