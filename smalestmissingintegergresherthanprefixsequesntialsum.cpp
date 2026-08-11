b89d9d25f698a256e9934422743d9d1a8646ede0
Anant Pratap <itsanant08@gmail.com>
2026-08-11 10:43:42 +0530

    Comments added

---
+++ smalestmissingintegergresherthanprefixsequesntialsum.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*for leetcode only */
+
+class Solution {
+public:
+    int missingInteger(vector<int>& nums) {
+        int sum = nums[0];
+
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] == nums[i - 1] + 1)
+                sum += nums[i];
+            else
+                break;
+        }
+
+        unordered_set<int> s(nums.begin(), nums.end());
+
+        while (s.count(sum))
+            sum++;
+
+        return sum;
+    }
+};
+
+int main() {
+    Solution obj;
+
+    vector<int> nums = {1, 2, 3, 2, 5};
+
+    cout << obj.missingInteger(nums);
+
+    return 0;
+}
