ead343f7228b492cfded82cc6a0f81d89bfd72e8
Anant Pratap <itsanant08@gmail.com>
2026-08-16 23:49:37 +0530

    Added reverse vowel code

---
+++ reversevowel.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+bool isVowel(char x)
+{
+    return x == 'a' || x == 'e' || x == 'i' || x == 'o' || x == 'u' ||
+           x == 'A' || x == 'E' || x == 'I' || x == 'O' || x == 'U';
+}
+
+int main()
+{
+    string s;
+    cin >> s;
+
+    int i = 0;
+    int j = s.length() - 1;
+
+    while(i < j)
+    {
+        while(i < j && !isVowel(s[i]))
+            i++;
+
+        while(i < j && !isVowel(s[j]))
+            j--;
+
+        swap(s[i], s[j]);
+
+        i++;
+        j--;
+    }
+
+    cout << s;
+
+    return 0;
+}
