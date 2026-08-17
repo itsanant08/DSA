ddf8ceff9ca7d892ec091e59c7c1b62f8349fca4
Anant Pratap <itsanant08@gmail.com>
2026-08-17 19:55:08 +0530

    deleted comments

---
+++ atoi.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <string>
+#include <climits>
+#include <cctype>
+using namespace std;
+
+int myAtoi(string s) {
+    int i = 0;
+    int n = s.length();
+
+    // Remove leading spaces
+    while (i < n && s[i] == ' ') {
+        i++;
+    }
+
+  
+    int sign = 1;
+
+    if (i < n && (s[i] == '+' || s[i] == '-')) {
+        if (s[i] == '-') {
+            sign = -1;
+        }
+        i++;
+    }
+
+    
+    long long num = 0;
+
+    while (i < n && isdigit(s[i])) {
+        num = num * 10 + (s[i] - '0');
+
+     
+        if (sign == 1 && num > INT_MAX) {
+            return INT_MAX;
+        }
+
+        if (sign == -1 && -num < INT_MIN) {
+            return INT_MIN;
+        }
+
+        i++;
+    }
+
+    return sign * num;
+}
+
+int main() {
+    string s;
+
+    cout << "Enter a string: ";
+    getline(cin, s);
+
+    cout << "Integer: " << myAtoi(s) << endl;
+
+    return 0;
+}
