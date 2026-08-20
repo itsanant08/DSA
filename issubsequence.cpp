9f3e0644132a1a5d5422c6ceb7ee1fa398d211dc
Anant Pratap <itsanant08@gmail.com>
2026-08-20 21:23:44 +0530

    Added is subsequence code

---
+++ issubsequence.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s, t;
+    cin >> s >> t;
+
+    int i = 0;
+
+    for (int j = 0; j < t.length(); j++) {
+        if (i < s.length() && s[i] == t[j]) {
+            i++;
+        }
+    }
+
+    if (i == s.length())
+        cout << "true";
+    else
+        cout << "false";
+
+    return 0;
+}
